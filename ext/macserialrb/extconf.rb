e41e5a1a90a1a86e9dc0daddc8af990eaa04fe3e
csrutil <csrutil@protonmail.com>
2020-11-26 22:17:27 +0800

    MISC - Tiny up code

--- ext/macserialrb/extconf.rb
+++ ext/macserialrb/extconf.rb
@@ -2,9 +2,9 @@ require "mkmf"
 
 case RUBY_PLATFORM
 when /linux/i
-  $CPPFLAGS += " -std=c11 -Werror -Wall"
+  $CPPFLAGS += " -std=c11 -Wall"
 when /darwin/i
-  $CPPFLAGS += " -std=c11 -Werror -Wall"
+  $CPPFLAGS += " -std=c11 -Wall"
 end
 
 create_makefile("macserialrb/macserialrb")
